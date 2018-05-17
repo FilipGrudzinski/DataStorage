271c79d0adb2fb63f77958da50aae1d363e7dc1f
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-05-17 22:00:52 +0200

    data

--- DataStorage/ViewController.swift
+++ DataStorage/ViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberField: UITextField!
+    
+    
+    @IBAction func numberButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(numberField.text, forKey: "number")
+        
+    }
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+        
+        if let number = numberObject as? String {
+            
+            numberField.text = number
+            
+            //numberLabel.text = number
+            
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
